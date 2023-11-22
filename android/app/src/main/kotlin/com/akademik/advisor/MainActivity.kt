5eba319882accb55288d47f093740b694e9e1274
MACBOOK DSI <maulana.malik@dwidasa.com>
2023-11-22 14:14:36 +0700

    change apk name and package name app

---
+++ android/app/src/main/kotlin/com/akademik/advisor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akademik.advisor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/spk_apk/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.spk_apk
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
